e3a681b776d1d39e206248591e1f2192e474cbf8
jaychen <jaychen@5xcampus.com>
2024-08-28 19:50:36 +0800

    issue#5 execute lint and push again

--- apps/products/migrations/0002_alter_product_price_alter_product_productnumber.py
+++ apps/products/migrations/0002_alter_product_price_alter_product_productnumber.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('products', '0001_initial'),
+        ("products", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='product',
-            name='price',
+            model_name="product",
+            name="price",
             field=models.IntegerField(),
         ),
         migrations.AlterField(
-            model_name='product',
-            name='productNumber',
+            model_name="product",
+            name="productNumber",
             field=models.CharField(max_length=10),
         ),
     ]
