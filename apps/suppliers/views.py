038b80999635243ccbdc81d0f5e1d520320d24a0
Michael <billbilltw@gmail.com>
2024-08-29 14:33:28 +0800

    feat: issue/#2 supplier model : update

--- apps/suppliers/views.py
+++ apps/suppliers/views.py
@@ -24,4 +24,19 @@ def new(req):
 
 def show(req, id):
     supplier = get_object_or_404(Supplier, pk=id)
+    if req.method == "POST":
+        form = SupplierForm(req.POST, instance=supplier)
+        if form.is_valid():
+            form.save()
+            return redirect("suppliers:show", supplier.id)
+        else:
+            return render(
+                req, "supplier/edit.html", {"supplier": supplier, "form": form}
+            )
     return render(req, "suppliers/show.html", {"supplier": supplier})
+
+
+def edit(req, id):
+    supplier = get_object_or_404(Supplier, pk=id)
+    form = SupplierForm(instance=supplier)
+    return render(req, "suppliers/edit.html", {"supplier": supplier, "form": form})
