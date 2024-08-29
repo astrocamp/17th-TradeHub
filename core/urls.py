7dba335c2dd3ad07ab5aa9353a2c78448fd00cba
KangPei <s522tw@gmail.com>
2024-08-30 03:27:16 +0800

    feat: issue/#1 create form component

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,9 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path("", include("apps.pages.urls")),
     path("admin/", admin.site.urls),
+    path("users/", include("apps.users.urls")),
     path("inventory/", include("apps.inventory.urls")),
 ] + debug_toolbar_urls()
