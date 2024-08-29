eb2f6c48b1be02aeee1926670e2900ddff7afd88
Michael <billbilltw@gmail.com>
2024-08-29 18:56:23 +0800

    feat: issue/#2 add checkbox to delete selected suppliers

--- apps/suppliers/urls.py
+++ apps/suppliers/urls.py
@@ -7,6 +7,7 @@ app_name = "suppliers"
 urlpatterns = [
     path("", views.index, name="index"),
     path("new", views.new, name="new"),
+    path("delete/many", views.delete_selected_suppliers, name="delete_selected"),
     path("show/<int:id>", views.show, name="show"),
     path("edit/<int:id>", views.edit, name="edit"),
     path("delete/<int:id>", views.delete, name="delete"),
