525d575e5a1d2e393382e8b1843f05a23213bcf3
Michael <billbilltw@gmail.com>
2024-08-30 00:44:13 +0800

    feat: issue/#10 purchase_order model : create

--- apps/purchase_orders/models.py
+++ apps/purchase_orders/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-from apps.suppliers.models import Supplier  # Import Supplier model from the suppliers app
 from django.utils import timezone
+
+from apps.suppliers.models import \
+    Supplier  # Import Supplier model from the suppliers app
+
 
 class PurchaseOrder(models.Model):
     order_number = models.CharField(max_length=20, unique=True)
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='purchase_orders')
+    supplier = models.ForeignKey(
+        Supplier, on_delete=models.CASCADE, related_name="purchase_orders"
+    )
     order_date = models.DateField(default=timezone.now)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     notes = models.TextField(blank=True, null=True)
--- apps/purchase_orders/urls.py
+++ apps/purchase_orders/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'purchase_orders'
+app_name = "purchase_orders"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("new", views.new, name="new"),
+    path("delete/many", views.delete_selected_purchase_orders, name="delete_selected"),
 ]
