8d7650ad95756b56a7f75b6042f9da21c0765393
toshi <toshimichi0809@hotmail.com>
2024-08-30 13:16:22 +0800

    fix: issue/#3 add uncommittted changes settings.py and urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,17 +2,10 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
 
-<<<<<<< HEAD
-
-urlpatterns = [
-    path("client/", include("apps.client.urls")),
-    path("products", include("apps.products.urls")),
-=======
-
-
 urlpatterns = [
     path("", include("apps.pages.urls")),
->>>>>>> 364e2f2 (Create Inventory model and startapp pages and add base.html)
     path("admin/", admin.site.urls),
     path("inventory/", include("apps.inventory.urls")),
+    path("client/", include("apps.client.urls")),
+    path("products", include("apps.products.urls")),
 ] + debug_toolbar_urls()
