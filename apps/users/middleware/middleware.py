989facdd240f69357b8cda00a8902468d822cc55
KangPei <s522tw@gmail.com>
2024-09-01 18:25:15 +0800

    chore: issue/#15 set up middleware

---
+++ apps/users/middleware/middleware.py
@@ -0,0 +1,23 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+class LoginRequiredMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        # 不用登入就能看到的頁面：登入、註冊、忘記密碼
+        exempt_urls = [
+            reverse("users:log_in"),
+            reverse("users:register"),
+            reverse("users:forget_password"),
+        ]
+
+        # 檢查用戶是否登錄
+        if not request.user.is_authenticated and request.path not in exempt_urls:
+            return redirect("users:log_in")
+
+        response = self.get_response(request)
+        return response
