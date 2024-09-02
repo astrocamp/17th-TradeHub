818bf3cb83b56699858a4dcd8c1cadfa6022eed4
jaychen <jaychen@5xcampus.com>
2024-09-02 15:15:04 +0800

    feat: issue/#11 renew index.html

--- apps/goods_receipts/forms/goods_receipt_form.py
+++ apps/goods_receipts/forms/goods_receipt_form.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
+
 from apps.goods_receipts.models import GoodsReceipt
+
 
 class GoodsReceiptForm(ModelForm):
     class Meta:
         model = GoodsReceipt
-        fields = ["receipt_number", "supplier", "goods_name", "quantity", "method", "date", "note"]
+        fields = [
+            "receipt_number",
+            "supplier",
+            "goods_name",
+            "quantity",
+            "method",
+            "date",
+            "note",
+        ]
