56e98eb9180f7e21f613c33f1877379b0d807f78
roger <r1990227@hotmail.com>
2024-09-04 14:50:08 +0800

    feat: issue/#19 add finite-state machine and sort and filter state for search field

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,26 +1,54 @@
 from django.db import models
+from django_fsm import FSMField, transition
 
 from apps.suppliers.models import Supplier
-
-# Create your models here.
 
 
 class Product(models.Model):
     product_id = models.CharField(max_length=10)
     product_name = models.CharField(max_length=20)
     price = models.IntegerField(null=False, blank=False)
-<<<<<<< HEAD
-    supplier = models.ManyToManyField(Supplier, related_name="products")
-    productNumber = models.CharField(max_length=10)
-    title = models.CharField(max_length=20)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
-=======
     supplier = models.ForeignKey(
         Supplier, on_delete=models.CASCADE, related_name="products", default=0
     )
->>>>>>> dev
     note = models.TextField()
 
     def __str__(self):
         return self.product_name
+
+    PRODUCT_STATE_OFTEN = "often"
+    PRODUCT_STATE_HAPLY = "haply"
+    PRODUCT_STATE_NEVER = "never"
+
+    PRODUCT_STATE_CHOICES = [
+        (PRODUCT_STATE_OFTEN, "經常"),
+        (PRODUCT_STATE_HAPLY, "偶爾"),
+        (PRODUCT_STATE_NEVER, "從不"),
+    ]
+
+    state = FSMField(
+        default=PRODUCT_STATE_OFTEN,
+        choices=PRODUCT_STATE_CHOICES,
+        protected=True,
+    )
+
+    def update_state(self):
+        pass
+
+    @transition(field=state, source="*", target=PRODUCT_STATE_NEVER)
+    def set_never(self):
+        pass
+
+    @transition(field=state, source="*", target=PRODUCT_STATE_HAPLY)
+    def set_haply(self):
+        pass
+
+    @transition(field=state, source="*", target=PRODUCT_STATE_OFTEN)
+    def set_often(self):
+        pass
+
+    def add_state(self):
+        pass
+
+    def remove_state(self):
+        pass
