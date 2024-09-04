cc644b82e55b44ca1913af00113dcb39a6521abb
KangPei <s522tw@gmail.com>
2024-09-04 16:05:25 +0800

    fix: issue/#15 fix middleware issue preventing password reset

--- apps/users/middleware/middleware.py
+++ apps/users/middleware/middleware.py
@@ -8,7 +8,7 @@ class LoginRequiredMiddleware:
 
     def __call__(self, request):
 
-        # 不用登入就能看到的頁面：登入、註冊、忘記密碼
+        # 不用登入就能看到的頁面
         exempt_urls = [
             reverse("users:log_in"),
             reverse("users:register"),
@@ -17,8 +17,13 @@ class LoginRequiredMiddleware:
 
         # 檢查用戶是否登錄
         if not request.user.is_authenticated and request.path not in exempt_urls:
-            if request.method == "POST" and request.path == reverse("users:index"):
-                return self.get_response(request)
+            if request.method == "POST":
+                # 檢查請求的路徑是否為特定的 URL，會先執行請求
+                if request.path in [
+                    reverse("users:index"),
+                    reverse("users:reset_password"),
+                ]:
+                    return self.get_response(request)
             return redirect("users:log_in")
 
         response = self.get_response(request)
