c8f4b6c405fa0d9aed6e3e046415a10a3f1b8640
roger <r1990227@hotmail.com>
2024-09-05 01:05:50 +0800

    feat: issue/#20 add finite-state machine / sort / filter state for search field

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -8,6 +8,7 @@ app_name = "clients"
 
 urlpatterns = [
     path("", DataListView.as_view(), name="list"),
+    path("index", views.index, name="index"),
     path("create", views.create, name="create"),
     path("edit/<int:id>", views.client_update_and_delete, name="edit"),
 ] + debug_toolbar_urls()
