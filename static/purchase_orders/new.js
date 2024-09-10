e4b3f9fbc5c8233e16d08e382edafa7a541f2346
Michael <billbilltw@gmail.com>
2024-09-10 19:25:50 +0800

    feat: issue/#22 dynamically create and delete

--- static/purchase_orders/new.js
+++ static/purchase_orders/new.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // 自動產生流水編號
     const generateOrderNumberUrl = window.djangoVariables.generateOrderNumberUrl;
 
     fetch(generateOrderNumberUrl)
@@ -9,10 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 orderNumberElement.textContent = data.order_number;
             }
         })
-        .catch(error => console.error('Error fetching order number:', error));
-});
 
-document.addEventListener('DOMContentLoaded', function() {
+    // 自動填入供應商資訊
     const supplierSelect = document.getElementById(window.djangoVariables.supplierIdField);
 
     supplierSelect.addEventListener('change', function() {
@@ -28,4 +27,45 @@ document.addEventListener('DOMContentLoaded', function() {
             })
             .catch(error => console.error('Error fetching supplier info:', error));
     });
+
+    // 子表單
+    const addItemButton = document.getElementById('add-item')
+    const formsetItems = document.getElementById('formset-items');
+    const totalForms = document.getElementById('id_items-TOTAL_FORMS');
+    let formCount = parseInt(totalForms.value);
+    addItemButton.addEventListener('click',() => {
+        const newItem = document.querySelector('#formset-items fieldset').cloneNode(true);
+        newItem.innerHTML = newItem.innerHTML.replace(/items-(\d+)-/g, `items-${formCount}-`);
+        newItem.querySelectorAll('input').forEach(input => input.value = '');
+        formsetItems.appendChild(newItem);
+        formCount++;
+        totalForms.value = formCount;
+    })
+    formsetItems.addEventListener('click', function (event) {
+        if (event.target.classList.contains('delete-item') && totalForms.value != 1) {
+            const fieldset = event.target.closest('fieldset');
+            fieldset.remove();
+            console.log(fieldset)
+            updateFormIndexes();
+        }
+    });
+    function updateFormIndexes() {
+        const formsets = formsetItems.querySelectorAll('fieldset');
+        formsets.forEach((formset, index) => {
+            formset.querySelectorAll('input, select').forEach((element) => {
+                const name = element.getAttribute('name');
+                const id = element.getAttribute('id');
+                if (name) {
+                    const newName = name.replace(/items-\d+-/, `items-${index}-`);
+                    element.setAttribute('name', newName);
+                }
+                if (id) {
+                    const newId = id.replace(/id_items-\d+-/, `id_items-${index}-`);
+                    element.setAttribute('id', newId);
+                }
+            });
+        });
+        totalForms.value = formsets.length;
+    }
 });
+
