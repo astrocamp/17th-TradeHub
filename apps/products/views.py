7f526cebae360e56aa723d375a34220be9155b7e
roger <r1990227@hotmail.com>
2024-09-21 01:59:52 +0800

    fix: issue/#71 add product cannot be repeated in stock

--- apps/products/views.py
+++ apps/products/views.py
@@ -213,11 +213,11 @@ def export_excel(request):
 @receiver(post_save, sender=Product)
 def update_state(sender, instance, **kwargs):
     time_now = datetime.now(timezone(timedelta(hours=+8))).strftime("%Y/%m/%d %H:%M:%S")
-
-    Inventory.objects.create(
-        product=instance,
-        supplier=instance.supplier,
-        quantity=0,
-        safety_stock=1,
-        note=f"{time_now} 新建商品，預建庫存",
-    )
+    if Inventory.objects.filter(product=instance).exists():
+        Inventory.objects.create(
+            product=instance,
+            supplier=instance.supplier,
+            quantity=0,
+            safety_stock=0,
+            note=f"{time_now} 新建商品，預建庫存",
+        )
