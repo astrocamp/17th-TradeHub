6feaabb54e1d3aec859e540ceb30d86eda7022b8
toshi <toshimichi0809@hotmail.com>
2024-09-21 13:07:41 +0800

    feat: issue/#74 remove information from navbar when user not login | add url to middleware.py

--- apps/users/middleware/middleware.py
+++ apps/users/middleware/middleware.py
@@ -9,6 +9,7 @@ class LoginRequiredMiddleware:
     def __call__(self, request):
 
         exempt_urls = [
+            reverse("pages:out_home"),
             reverse("users:log_in"),
             reverse("users:register"),
             reverse("users:forget_password"),
