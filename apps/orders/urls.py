2cd9b6810261e7defa66a21767b8f10b9a6eb2aa
toshi <toshimichi0809@hotmail.com>
2024-09-21 17:34:18 +0800

    issue/#73 delete every orders import_file url

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("new", views.new, name="new"),
     path("edit/<int:id>", views.order_update_and_delete, name="edit"),
-    path("import", views.import_file, name="import_file"),
     path("export_csv", views.export_csv, name="export_csv"),
     path("export_excel", views.export_excel, name="export_excel"),
 ]
