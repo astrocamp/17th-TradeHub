d1082de7590e36f45c4c608ec8470ccef3dc0920
Michael <billbilltw@gmail.com>
2024-09-25 11:35:38 +0800

    fix: issue/#91 fix client_number

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -7,6 +7,7 @@ app_name = "clients"
 urlpatterns = [
     path("", views.index, name="index"),
     path("new", views.new, name="new"),
+    # path("show", views.show, name="show"),
     path("edit/<int:id>", views.client_update_and_delete, name="edit"),
     path("import", views.import_file, name="import_file"),
     path("export_excel", views.export_excel, name="export_excel"),
