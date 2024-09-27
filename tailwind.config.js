88f78622ddc0f426b353c69c3f2c9be58a4a82a7
KangPei <s522tw@gmail.com>
2024-09-27 12:32:54 +0800

    style: issue/#97 update purchase_orders index rwd

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      width: {
+        '85p': '85%',
+        '90p': '90%',
+        '80p': '80%',
+      }
     },
   },
   plugins: [require("daisyui")],
