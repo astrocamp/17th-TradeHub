f989ac097f5dcf619b70a84647cb3cb3c664193a
KangPei <s522tw@gmail.com>
2024-09-28 09:35:29 +0800

    feat: issue/#97-1 notification adjust and font-size update

--- apps/users/context_processors.py
+++ apps/users/context_processors.py
@@ -3,7 +3,7 @@ from .models import Notification
 
 def unread_count(request):
     if request.user.is_authenticated:
-        count = Notification.objects.filter(is_read=False, user=request.user).count()
+        count = Notification.objects.filter(is_read=False).count()
     else:
         count = 0
     return {"unread_count": count}
